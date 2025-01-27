b4c587b4ca2752bd855e21ef3ac50c0072d15f8d
Gustavo <gustavohn2017@gmail.com>
2025-01-27 10:51:06 -0300

    First Commit

--- src/main.js
+++ src/main.js
@@ -1,16 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const tabs = document.querySelectorAll(".menu__tab");
-    const contents = document.querySelectorAll(".menu__content");
+    const tabs = document.querySelectorAll(".tab-menu__tab");
+    const contents = document.querySelectorAll(".tab-menu__content");
 
-    tabs.forEach(tab => {
-        tab.addEventListener("click", function() {
-            // Remove active class from all tabs and contents
-            tabs.forEach(t => t.classList.remove("menu__tab--active"));
-            contents.forEach(c => c.classList.remove("menu__content--active"));
-
-            // Add active class to clicked tab and corresponding content
-            tab.classList.add("menu__tab--active");
-            document.querySelector(`.menu__content[data-content="${tab.dataset.tab}"]`).classList.add("menu__content--active");
+    tabs.forEach((tab, index) => {
+        tab.addEventListener("click", function() {           
+            tabs.forEach(t => t.classList.remove("tab-menu__tab--active"));
+            contents.forEach(c => c.classList.remove("tab-menu__content--active"));
+            tab.classList.add("tab-menu__tab--active");
+            contents[index].classList.add("tab-menu__content--active");
         });
     });
 });
